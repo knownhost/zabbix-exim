110c6d45b26249cae0476ab0c35efa5848c59edb
Jonathan Wright <jonathan@effecthost.com>
2018-05-14 12:38:14 -0500

    Update zabbix-exim-linux.sh

--- zabbix-exim-linux.sh
+++ zabbix-exim-linux.sh
@@ -26,11 +26,11 @@ $EXIMSTATS -t0 -nvr $TMP1 > $TMP2
 
 echo "Errors 0 0" >> $TMP2
 
-zsend exreceived `grep -m 1 Received $TMP2|awk '{print $3}'`
-zsend exdelivered `grep -m 1 Delivered $TMP2|awk '{print $3}'`
-zsend exerrors `grep -m 1 Errors $TMP2|awk '{print $3}'`
-zsend exbytesreceived `grep -m 1 "Received" $TMP2|awk '{print $2}'`
-zsend exbytesdelivered `grep -m 1 "Delivered" $TMP2|awk '{print $2}'`
+zsend exreceived `grep -m 1 Received $TMP2|awk '{if (!NR) print 0; else print $3}'`
+zsend exdelivered `grep -m 1 Delivered $TMP2|awk '{if (!NR) print 0; else print $3}'`
+zsend exerrors `grep -m 1 Errors $TMP2|awk '{if (!NR) print 0; else print $3}'`
+zsend exbytesreceived `grep -m 1 "Received" $TMP2|awk '{if (!NR) print 0; else print $2}'`
+zsend exbytesdelivered `grep -m 1 "Delivered" $TMP2|awk '{if (!NR) print 0; else print $2}'`
 zsend exoutbound `sed -e '/Deliveries by transport/,/Messages received per hour/!d' $TMP2 |grep remote_smtp |awk '{sum += $3} END {if (!NR) print 0; else print sum}'`
 zsend exinbound `sed -e '/Deliveries by transport/,/Messages received per hour/!d' $TMP2 |grep -E "(dovecot|cagefs_virtual_address_pipe)" |awk '{sum += $3} END {if (!NR) print 0; else print sum}'`
 zsend exmailqueue `exim -bpc`
